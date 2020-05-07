964ae12b7f13f28ac07baae2060d09276a1496df
Magda <magdabaj@protonmail.com>
2020-05-07 12:22:45 +0200

    dd

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const users = require('./server/users');
+// const users = require('./server/users');
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/users', users);
+// app.use('/users', users);
 
 app.get('/ping', function (req, res) {
     return res.send('pong');
