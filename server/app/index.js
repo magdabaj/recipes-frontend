d659cdf137392fa9835e1e2a76cf0719afe695b7
Magda <magdabaj@protonmail.com>
2020-05-07 13:58:51 +0200

    server

--- server/app/index.js
+++ server/app/index.js
@@ -16,7 +16,12 @@ app.use(express.static(path.join(__dirname, '../../dist')));
 
 // adding the frontend origin
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://afternoon-plateau-23579.herokuapp.com/', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://afternoon-plateau-23579.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cookieParser());
 
 app.use('/recipe', recipeRouter);
