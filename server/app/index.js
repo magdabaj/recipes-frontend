3adbd4b74da950e6036b1a3f36e4d4d80df1bf66
Magda <magdabaj@protonmail.com>
2020-05-10 15:49:20 +0200

    server

--- server/app/index.js
+++ server/app/index.js
@@ -17,11 +17,6 @@ const app = express();
 // adding the frontend origin
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://afternoon-plateau-23579.herokuapp.com/"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use(cookieParser());
 
 app.use('/recipe', recipeRouter);
--- server/secrets/databaseConfiguration.js
+++ server/secrets/databaseConfiguration.js
@@ -11,4 +11,5 @@ module.exports = {
   database: 'd345ig2ibrppp0',
   password: 'ffd888d8baae6f461de2d0ce845ea04fced01a4da4178a33acf19680b1f2e729',
   port: 5432,
+  sslmode: require
 };
